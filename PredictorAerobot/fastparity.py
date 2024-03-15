41de8457477775c09b5139927ed31ad623ca8a8b
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-15 23:59:11 +0530

    Update fastparity.py

--- PredictorAerobot/fastparity.py
+++ PredictorAerobot/fastparity.py
@@ -1,16 +1,15 @@
 from Data import Data
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardMarkup, Message 
 from Data import DFASTPARITY_IMG
 
 # Start Message
 @Client.on_message(filters.text & filters.incoming & filters.command("fastparity"))
 @Client.on_message(filters.private & filters.incoming & filters.command("fastparity"))
 @Client.on_message(filters.text & filters.incoming & filters.command("fastparity@PredictorAerobot"))
-async def start(bot, msg):
+async def fastparity(bot, msg):
 	user = await bot.get_me()
 	mention = user.mention
 	await msg.reply_photo(DFASTPARITY_IMG,
 	caption=Data.FASTPARITY.format(msg.from_user.mention, mention),
-	reply_markup=InlineKeyboardMarkup(Data.gamebuttons)
-                       )
+	reply_markup=InlineKeyboardMarkup(Data.gamebuttons), quote=True)
