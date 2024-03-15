b5533c2b515107687725bba367289862cd170e32
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-15 23:07:24 +0530

    Create fastparity.py

---
+++ PredictorAerobot/fastparity.py
@@ -0,0 +1,16 @@
+from Data import Data
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup
+from Config import START_IMG
+
+# Start Message
+@Client.on_message(filters.text & filters.incoming & filters.command("fastparity"))
+@Client.on_message(filters.private & filters.incoming & filters.command("fastparity"))
+@Client.on_message(filters.text & filters.incoming & filters.command("fastparity@PredictorAerobot"))
+async def start(bot, msg):
+	user = await bot.get_me()
+	mention = user.mention
+	await msg.reply_photo(START_IMG,
+	caption=Data.FASTPARITY.format(msg.from_user.mention, mention),
+	reply_markup=InlineKeyboardMarkup(Data.buttons)
+                       )
