e63173c193125abf76ec4432ce060a628ab23def
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-15 21:06:59 +0530

    Update help.py

--- PredictorAerobot/help.py
+++ PredictorAerobot/help.py
@@ -5,8 +5,7 @@ from Config import START_IMG
 
 # Help Message
 @Client.on_message(filters.private & filters.incoming & filters.command("help"))
-@Client.on_message(filters.text & filters.incoming & filters.command("help@Aero_Force2_Subscriber_Bot"))
-@Client.on_message(filters.text & filters.incoming & filters.command("help@Aero_Force_Subscriber_Bot"))
+@Client.on_message(filters.text & filters.incoming & filters.command("help@PredictorAerobot"))
 @Client.on_message(filters.text & filters.incoming & filters.command("help"))
 async def _help(bot, msg):
     await msg.reply_photo(
