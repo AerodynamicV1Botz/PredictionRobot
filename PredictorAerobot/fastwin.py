4a50d4eccc2cd753f2735d4afb17dd868a0d0318
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-15 21:09:14 +0530

    Update fastwin.py

--- PredictorAerobot/fastwin.py
+++ PredictorAerobot/fastwin.py
@@ -5,8 +5,7 @@ from Config import START_IMG
 
 # Fastwin Message
 @Client.on_message(filters.private & filters.incoming & filters.command("fastwin"))
-@Client.on_message(filters.text & filters.incoming & filters.command("fastwin@Aero_Force2_Subscriber_Bot"))
-@Client.on_message(filters.text & filters.incoming & filters.command("fastwin@Aero_Force_Subscriber_Bot"))
+@Client.on_message(filters.text & filters.incoming & filters.command("fastwin@PredictorAerobot"))
 @Client.on_message(filters.text & filters.incoming & filters.command("fastwin"))
 async def _fastwin(bot, msg):
     await msg.reply_photo(
