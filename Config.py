9c7f6b59ba5723a5962845df966df5248a911b47
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-16 08:53:17 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -13,6 +13,21 @@ if ENVIRONMENT:
     API_HASH = os.environ.get('API_HASH', "c408c6b40e1ebd04b76c7d04a8de1dad")
     BOT_TOKEN = os.environ.get('BOT_TOKEN', "5658314356:AAEIWcIyXBsQhRrltN_Nki6Ra5GcrTY522s")
     DATABASE_URL = os.environ.get('DATABASE_URL', None)
+    # SUDO USERS
+SUDO_USER = os.environ.get("SUDO_USER", "").split())
+)  # Input type must be interger
+
+# You'll need a Private Group ID for this.
+LOG_GROUP_ID = os.environ.get("LOG_GROUP_ID"))
+
+# Message to display when someone starts your bot
+PRIVATE_START_MESSAGE = os.environ.get(
+    "PRIVATE_START_MESSAGE",
+    "Hello! Welcome to my Personal Assistant Bot",
+)
+
+# Database to save your chats and stats... Get MongoDB:-  https://notreallyshikhar.gitbook.io/yukkimusicbot/deployment/mongodb#4.-youll-see-a-deploy-cloud-database-option.-please-select-shared-hosting-under-free-plan-here
+MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     START_IMG = os.environ.get('START_IMG', None)
     BOT_USERNAME = os.environ.get('BOT_USERNAME', None)
     OWNER_ID=1484735126
