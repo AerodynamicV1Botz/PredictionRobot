18a01051b4dc6cca8cb1e29afb655966c3779910
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-18 21:18:38 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -33,6 +33,57 @@ FASTPARITY_IMG = [
 "https://graph.org/file/5b1abf3d89f410dcd5abc.jpg",
 ]
 
+FUNWIN_IMG = [
+"https://graph.org/file/e1b3936d257a1b408b2fb.jpg",
+"https://graph.org/file/e1b3936d257a1b408b2fb.jpg",
+]
+
+FUNWINPARITY_IMG = [
+"https://graph.org/file/6ee7466ebbec46a6fe80f.jpg"
+"https://graph.org/file/f1e9800539af3154b3ac5.jpg",
+"https://graph.org/file/349469066378c1c1b27ba.jpg",
+"https://graph.org/file/ed0bf655df070867a98b0.jpg",
+"https://graph.org/file/7e8337d909f9829b56003.jpg",
+"https://graph.org/file/bb551518253815c5deb3e.jpg",
+"https://graph.org/file/f945a2a251a7d2ae7dcc8.jpg",
+"https://graph.org/file/ed0bf655df070867a98b0.jpg",
+"https://graph.org/file/ff9315a6a5847b6d9e7c2.jpg",
+"https://graph.org/file/7c14028a211908f82f2c4.jpg",
+"https://graph.org/file/90bd67ea9655caee38fee.jpg",
+"https://graph.org/file/6527972ee1ca626c199e9.jpg",
+"https://graph.org/file/17213e0f9078e4c4c1bcb.jpg",
+"https://graph.org/file/4f78b1055639de8ff5b43.jpg",
+"https://graph.org/file/ecb9bfffc1883fae1ec96.jpg",
+"https://graph.org/file/105c3d5c4390bee5992cf.jpg",
+"https://graph.org/file/75c44e33848cd82586c89.jpg",
+"https://graph.org/file/ec6ea1c24f05775839352.jpg",
+"https://graph.org/file/569ace5488aefa50891f7.jpg",
+"https://graph.org/file/f7171224427541ab9d54d.jpg",
+]
+
+FUNWINFASTPARITY_IMG = [
+"https://graph.org/file/f1a03f7349a136cb7f1e1.jpg",
+"https://graph.org/file/08858d76afa4afd257ee0.jpg",
+"https://graph.org/file/7f9462843e93eb2bb7b40.jpg",
+"https://graph.org/file/9065b78c6bcac98f9221c.jpg",
+"https://graph.org/file/a6ea3c11261edaba9ac6b.jpg",
+"https://graph.org/file/7099b879a81827f8e061a.jpg",
+"https://graph.org/file/882445ad9131b02b3f54c.jpg",
+"https://graph.org/file/5a44d2259d253aada769c.jpg",
+"https://graph.org/file/65423793ea670254c7a38.jpg",
+"https://graph.org/file/3eb8c038f31a3239ad184.jpg",
+"https://graph.org/file/30e0c426c988eacd75782.jpg",
+"https://graph.org/file/5a44d2259d253aada769c.jpg",
+"https://graph.org/file/01330607ef98ccc4254f1.jpg",
+"https://graph.org/file/15d4b7e695974d8b819a0.jpg",
+"https://graph.org/file/cd8bedbce8db82a80d9e2.jpg",
+"https://graph.org/file/b49c4303e347182243b0e.jpg",
+"https://graph.org/file/d74152e0f477feed5054a.jpg",
+"https://graph.org/file/b0d3c455ded88effbf979.jpg",
+"https://graph.org/file/a713d3daeb8a6f2e3a81a.jpg",
+"https://graph.org/file/3b00713d66156de3de9a6.jpg",
+]
+
 class Data:
     # Start Message
     START = """
