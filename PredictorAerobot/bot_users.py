740baea719be2dac2ba803504ce92c057a43bb44
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-19 22:52:35 +0530

    Update bot_users.py

--- PredictorAerobot/bot_users.py
+++ PredictorAerobot/bot_users.py
@@ -8,7 +8,7 @@ from PredictorAerobot.misc import SUDOERS
 from Config import OWNER_ID
 
 @Client.on_message( ~filters.service, group=1)
-async def users_sql(_, bot, msg: Message):
+async def users_sql(_, client, msg: Message):
     if msg.from_user:
         q = SESSION.query(Users).get(int(msg.from_user.id))
         if not q:
@@ -20,7 +20,7 @@ async def users_sql(_, bot, msg: Message):
 
 @Client.on_message(filters.user(OWNER_ID) & filters.command("stats") & SUDOERS)
 @Client.on_message(filters.user(OWNER_ID) & filters.command("stats@PredictorAerobot") & SUDOERS)
-async def _stats(_, bot, msg: Message):
+async def _stats(_, client, msg: Message):
     users = await num_users()
     chats = await num_chats()
     await msg.reply(f"**About This Bot** \n\nThis is Aero ✘ Force Subscriber~🇮🇳 \nA powerful Telegram subscribing bot to force users in your group to join a particular chat. \n──────────────────── \n➻ Users ≈ {users} \n➻ Chats ≈ 1{chats} \n──────────────────── \n★Network » @AerodynamicV1Botz \n★Developer » @AerodynamicV1_OFFICIAL \n★Update » @AerodynamicV1_UPDATE \n★Support » @AerodynamicV1_SUPPORT \n★Free Promotion » @AerodynamicV1_Promotion", quote=True)
