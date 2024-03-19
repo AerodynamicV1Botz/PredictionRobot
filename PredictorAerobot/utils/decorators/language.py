0d9bf1b74d35adf0cf81132ec7ffa7ae649817c9
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-19 20:02:09 +0530

    Update language.py

--- PredictorAerobot/utils/decorators/language.py
+++ PredictorAerobot/utils/decorators/language.py
@@ -1,5 +1,5 @@
-from EsproMusic.misc import SUDOERS
-from EsproMusic.utils.database import get_lang, is_maintenance
+from PredictorAerobot.misc import SUDOERS
+from PredictorAerobot.utils.database import get_lang, is_maintenance
 from strings import get_string
 
 
