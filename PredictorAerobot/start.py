bafcbd7fe86f6df312c798f8de6ba95eec125505
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-20 22:58:21 +0530

    Update start.py

--- PredictorAerobot/start.py
+++ PredictorAerobot/start.py
@@ -1,6 +1,6 @@
 from Data import Data
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto
 
 # Start Message
 @Client.on_message(filters.group & filters.command("start"))
