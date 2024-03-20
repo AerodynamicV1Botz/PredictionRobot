1ad7033b12f689e5d7c6cf9e280d3a285e76aa5e
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-20 20:51:37 +0530

    Create payment.py

---
+++ PredictorAerobot/payment.py
@@ -0,0 +1,16 @@
+from Data import Data
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message 
+from Data import PAYMENT_IMG
+import random 
+
+# Start Message
+@Client.on_message(filters.text & filters.incoming & filters.command("payment"))
+@Client.on_message(filters.private & filters.incoming & filters.command("payment"))
+@Client.on_message(filters.text & filters.incoming & filters.command("payment@PredictorAerobot"))
+async def payment(bot, msg):
+	await msg.reply_photo(
+	random.choice(PAYMENT_IMG),
+	caption=DATA.PAYMENT,
+	reply_markup=InlineKeyboardMarkup(Data.paymentbuttons)
+  )
