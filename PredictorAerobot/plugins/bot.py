6b9f84f5428627308c86ee6941ddc0694803a3e7
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-20 09:26:23 +0530

    Update bot.py

--- PredictorAerobot/plugins/bot.py
+++ PredictorAerobot/plugins/bot.py
@@ -16,9 +16,9 @@ sudousers = os.environ.get("SUDO_USER", None)
 # this Feature Will Works only If u r Added Heroku api
 @Client.on_message(filters.user(OWNER_ID) & filters.user(SUDO_USERS) & filters.command("addsudo"))
 @Client.on_message(filters.user(OWNER_ID) & filters.user(SUDO_USERS) & filters.command("addsudo@PredictorAerobot"))
-async def tb(_, msg: Message):
-    if msg.sender_id == OWNER_ID:
-        ok = await msg.reply("Adding user as a sudo...")
+async def tb(event):
+    if event.sender_id == OWNER_ID:
+        ok = await event.reply("Adding user as a sudo...")
         AERO = "SUDO_USER"
         if HEROKU_APP_NAME is not None:
             app = Heroku.app(HEROKU_APP_NAME)
