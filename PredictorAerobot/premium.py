bf0f052c458fbdb67755e60c7d0fc9d6f6fde2b3
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-20 22:36:39 +0530

    Update premium.py

--- PredictorAerobot/premium.py
+++ PredictorAerobot/premium.py
@@ -1,7 +1,7 @@
 from Data import Data
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message 
-from Config import START_IMG
+from Data import START_IMG
 import random 
 
 # Start Message
