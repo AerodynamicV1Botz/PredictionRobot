e68c4a390618bd11f351418150a83d20f7087e2e
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-21 14:17:34 +0530

    Update callbacks.py

--- PredictorAerobot/callbacks.py
+++ PredictorAerobot/callbacks.py
@@ -102,10 +102,10 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
     elif query == "payment":
         chat_id = callback_query.from_user.id
         message_id = callback_query.message.id
-        await bot.send_photo(
+        await bot.edit_message_text(
             chat_id=chat_id,
             message_id=message_id,
-            photo=PAYMENTS_IMG,
+            reply_photo=PAYMENTS_IMG,
             text=Data.PAYMENT,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.paymentbuttons),
