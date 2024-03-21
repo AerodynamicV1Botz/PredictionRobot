e82393e8321b29a420d7bfc7b68be278a164c689
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-21 14:35:00 +0530

    Update callbacks.py

--- PredictorAerobot/callbacks.py
+++ PredictorAerobot/callbacks.py
@@ -1,5 +1,4 @@
 from Data import Data
-from Data import PAYMENTS_IMG
 from pyrogram import Client
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InputMediaPhoto
@@ -102,9 +101,7 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
     elif query == "payment":
         chat_id = callback_query.from_user.id
         message_id = callback_query.message.id
-        await bot.send_photo(
-            photo=PAYMENTS_IMG,)
-            bot.edit_message_text(
+        await bot.edit_message_text(
             chat_id=chat_id,
             message_id=message_id,
             text=Data.PAYMENT,
