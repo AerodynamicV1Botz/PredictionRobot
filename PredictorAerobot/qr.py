661ee13e65fafa0bfc1e40cbecd21748f3babe66
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-21 14:44:01 +0530

    Create qr.py

---
+++ PredictorAerobot/qr.py
@@ -0,0 +1,16 @@
+from Data import Data
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message 
+from Data import PAYMENT_IMG
+import random 
+
+# Start Message
+@Client.on_message(filters.text & filters.incoming & filters.command("qr"))
+@Client.on_message(filters.private & filters.incoming & filters.command("qr"))
+@Client.on_message(filters.text & filters.incoming & filters.command("qr@PredictorAerobot"))
+async def qr(bot, msg):
+	await msg.reply_photo(
+	random.choice(PAYMENT_IMG),
+	caption=Data.QR,
+	reply_markup=InlineKeyboardMarkup(Data.qrbuttons)
+  )
