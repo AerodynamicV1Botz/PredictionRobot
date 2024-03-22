ae6510746262ab347334ea66d2762ec15c21e009
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-22 13:41:15 +0530

    Update bmwingo.py

--- PredictorAerobot/bmwingo.py
+++ PredictorAerobot/bmwingo.py
@@ -6,3 +6,23 @@ import random
 from Config import OWNER_ID, SUDO_ID, SUDO_ID1, SUDO_ID2, SUDO_ID3
 
 @Client.on_message(filters.user([OWNER_ID, SUDO_ID, SUDO_ID1, SUDO_ID2, SUDO_ID3]) & filters.command([
+
+  
+  
+  
+  
+  
+  
+
+  
+  
+  
+  
+async def funwinfast(bot, msg):
+	user = await bot.get_me()
+	mention = user.mention
+	await msg.reply_photo(
+	random.choice(FUNWINFASTPARITY_IMG),
+	caption=Data.FUNWINFAST.format(msg.from_user.mention, mention),
+	reply_markup=InlineKeyboardMarkup(Data.gamebuttons)
+  )
