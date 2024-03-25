663d21083b97079ab5090a9c893f0895e1d35259
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-25 14:47:21 +0530

    Update premium.py

--- PredictorAerobot/premium.py
+++ PredictorAerobot/premium.py
@@ -1,15 +1,24 @@
 from Data import Data
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message 
-from Data import START_IMG
+from Config import START_IMG
 import random 
 
 # Start Message
-@Client.on_message(filters.text & filters.incoming & filters.command("premium"))
-@Client.on_message(filters.private & filters.incoming & filters.command("premium"))
-@Client.on_message(filters.text & filters.incoming & filters.command("premium@PredictorAerobot"))
+@Client.on_message(filters.private & filters.command("premium"))
+@Client.on_message(filters.private & filters.command("premium@PredictorAerobot"))
 async def premium(bot, msg):
 	await msg.reply_photo(START_IMG,
 	caption=Data.PREMIUM,
 	reply_markup=InlineKeyboardMarkup(Data.paymemthome)
   )
+
+# Start Message
+@Client.on_message(filters.group & filters.command("premium"))
+@Client.on_message(filters.group & filters.command("premium@PredictorAerobot"))
+async def premium(bot, msg):
+	await msg.reply_photo(START_IMG,
+	caption=Data.PREMIUM,
+	reply_markup=InlineKeyboardMarkup(Data.paymemthome)
+			     )
+	
