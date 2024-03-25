ca6e1efa1c0ba398cea16acc87809be2061cadd1
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-25 17:03:35 +0530

    Update fastparity.py

--- PredictorAerobot/fastparity.py
+++ PredictorAerobot/fastparity.py
@@ -13,16 +13,5 @@ async def fastparity(bot, msg):
 	await msg.reply_photo(
 	random.choice(DFASTPARITY_IMG),
 	caption=Data.FASTPARITY.format(msg.from_user.mention, mention),
-	reply_markup=InlineKeyboardMarkup(Data.gamebuttons)
-	)
-# Start Message
-@Client.on_message(filters.group & filters.command("fastparity"))
-@Client.on_message(filters.group & filters.command("fastparity@PredictorAerobot"))
-async def fastparity(bot, msg):
-	user = await bot.get_me()
-	mention = user.mention
-	await msg.reply_photo(
-	random.choice(DFASTPARITY_IMG),
-	caption=Data.FASTPARITY.format(msg.from_user.mention, mention),
-	reply_markup=InlineKeyboardMarkup(Data.gamebuttons)
+	reply_markup=InlineKeyboardMarkup(Data.fastwin_back)
 	)
