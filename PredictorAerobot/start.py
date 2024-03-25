14171dbffbe2876cf40272c91c0230dba886fd7d
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-25 19:02:44 +0530

    Update start.py

--- PredictorAerobot/start.py
+++ PredictorAerobot/start.py
@@ -15,7 +15,7 @@ async def start(bot, msg):
 	reply_markup=InlineKeyboardMarkup(Data.start_group_buttons),
         )
 
-@Client.on_message(filters.private & filters.incoming filters.command("start"))
+@Client.on_message(filters.private & filters.incoming & filters.command("start"))
 @Client.on_message(filters.private & filters.incoming & filters.command("start@PredictorAerobot"))
 async def start(bot, msg):
 	user = await bot.get_me()
