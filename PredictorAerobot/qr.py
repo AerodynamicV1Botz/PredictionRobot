6178e3b2ce225a12ea1e22b6896474832c984e8d
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-25 18:43:01 +0530

    Update qr.py

--- PredictorAerobot/qr.py
+++ PredictorAerobot/qr.py
@@ -1,7 +1,7 @@
 from Data import Data
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message 
-from Aero import PAYMENT_IMG
+from Data import PAYMENT_IMG
 import random 
 
 # Start Message
