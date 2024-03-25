0becb245531a97e5217c35d51ee8d95b7421e787
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-25 16:54:49 +0530

    Create chat.py

---
+++ PredictorAerobot/chat.py
@@ -0,0 +1,16 @@
+from Data import Data
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup
+from Data import START_IMG
+
+# Help Message
+@Client.on_message(filters.private & filters.command("chat@PredictorAerobot"))
+@Client.on_message(filters.private & filters.command("chat"))
+async def chat(bot, msg):
+    await msg.reply_photo(
+        START_IMG,
+        caption=Data.CHAT,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(Data.about_back)
+    )
+  
