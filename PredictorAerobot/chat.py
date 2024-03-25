7a2ba70b849ffc47e2c2a2a797be50e21487a2ae
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-25 18:46:26 +0530

    Update chat.py

--- PredictorAerobot/chat.py
+++ PredictorAerobot/chat.py
@@ -7,8 +7,7 @@ from Config import START_IMG
 @Client.on_message(filters.private & filters.command("chat@PredictorAerobot"))
 @Client.on_message(filters.private & filters.command("chat"))
 async def chat(bot, msg):
-    await msg.reply_photo(
-        START_IMG,
+    await msg.reply_photo(START_IMG,
         caption=Data.CHAT,
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(Data.about_back)
