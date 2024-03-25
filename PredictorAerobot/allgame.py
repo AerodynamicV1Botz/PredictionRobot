b8fbee4ed31285a5224ac80407d447b1365f2a74
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-25 16:45:01 +0530

    Update allgame.py

--- PredictorAerobot/allgame.py
+++ PredictorAerobot/allgame.py
@@ -5,11 +5,19 @@ from Config import START_IMG
 import random 
 
 # Start Message
-@Client.on_message(filters.text & filters.incoming & filters.command("allgame"))
-@Client.on_message(filters.private & filters.incoming & filters.command("allgame"))
-@Client.on_message(filters.text & filters.incoming & filters.command("allgame@PredictorAerobot"))
+@Client.on_message(filters.private & filters.command("allgame"))
+@Client.on_message(filters.private & filters.command("allgame@PredictorAerobot"))
 async def allgame(bot, msg):
 	await msg.reply_photo(START_IMG,
 	caption=Data.ALLGAME,
 	reply_markup=InlineKeyboardMarkup(Data.allgamebuttons)
   )
+# Start Message
+@Client.on_message(filters.group & filters.command("allgame"))
+@Client.on_message(filters.group & filters.command("allgame@PredictorAerobot"))
+async def allgame(bot, msg):
+	await msg.reply_photo(START_IMG,
+	caption=Data.ALLGAME,
+	reply_markup=InlineKeyboardMarkup(Data.allgamebuttons)
+        )
+	
