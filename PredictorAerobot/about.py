4fa664e9344081c7665c59109e37cb6acf6aacce
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-25 18:46:12 +0530

    Update about.py

--- PredictorAerobot/about.py
+++ PredictorAerobot/about.py
@@ -7,8 +7,7 @@ from Config import START_IMG
 @Client.on_message(filters.private & filters.command("about@PredictorAerobot"))
 @Client.on_message(filters.private & filters.command("about"))
 async def _about(bot, msg):
-    await msg.reply_photo(
-        START_IMG,
+    await msg.reply_photo(START_IMG,
         caption=Data.ABOUT,
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(Data.about_buttons)
