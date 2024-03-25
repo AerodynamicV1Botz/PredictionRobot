1f142e90ed36ba4c95ff2e9d811e9cf2c74ef5df
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-25 16:56:20 +0530

    Update premium.py

--- PredictorAerobot/premium.py
+++ PredictorAerobot/premium.py
@@ -12,13 +12,3 @@ async def premium(bot, msg):
 	caption=Data.PREMIUM,
 	reply_markup=InlineKeyboardMarkup(Data.paymemthome)
   )
-
-# Start Message
-@Client.on_message(filters.group & filters.command("premium"))
-@Client.on_message(filters.group & filters.command("premium@PredictorAerobot"))
-async def premium(bot, msg):
-	await msg.reply_photo(START_IMG,
-	caption=Data.PREMIUM,
-	reply_markup=InlineKeyboardMarkup(Data.paymemthome)
-			     )
-	
