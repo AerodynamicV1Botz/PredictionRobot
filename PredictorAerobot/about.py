10413b49298b1ad39e105b0d99b76e16b39cbb38
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-25 16:46:30 +0530

    Update about.py

--- PredictorAerobot/about.py
+++ PredictorAerobot/about.py
@@ -4,13 +4,23 @@ from pyrogram.types import InlineKeyboardMarkup
 from Data import START_IMG
 
 # Help Message
-@Client.on_message(filters.private & filters.incoming & filters.command("about"))
-@Client.on_message(filters.text & filters.incoming & filters.command("about@PredictorAerobot"))
-@Client.on_message(filters.text & filters.incoming & filters.command("about"))
+@Client.on_message(filters.private & filters.command("about@PredictorAerobot"))
+@Client.on_message(filters.private & filters.command("about"))
 async def _about(bot, msg):
     await msg.reply_photo(
         START_IMG,
         caption=Data.ABOUT,
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(Data.home_buttons)
-    )
+        )
+# Help Message
+@Client.on_message(filters.group & filters.command("about@PredictorAerobot"))
+@Client.on_message(filters.group & filters.command("about"))
+async def _about(bot, msg):
+    await msg.reply_photo(
+        START_IMG,
+        caption=Data.ABOUT,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(Data.home_buttons)
+        )
+    
