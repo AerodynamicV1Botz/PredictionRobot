771716920dfcfb7f5531a3a46e773473c5975d74
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-25 14:48:32 +0530

    Update payment.py

--- PredictorAerobot/payment.py
+++ PredictorAerobot/payment.py
@@ -1,16 +1,26 @@
 from Data import Data
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message 
-from Data import PAYMENT_IMG
+from Aero import PAYMENT_IMG
 import random 
 
 # Start Message
-@Client.on_message(filters.text & filters.incoming & filters.command("payment"))
-@Client.on_message(filters.private & filters.incoming & filters.command("payment"))
-@Client.on_message(filters.text & filters.incoming & filters.command("payment@PredictorAerobot"))
+@Client.on_message(filters.private & filters.command("payment"))
+@Client.on_message(filters.private & filters.command("payment@PredictorAerobot"))
 async def payment(bot, msg):
 	await msg.reply_photo(
 	random.choice(PAYMENT_IMG),
 	caption=Data.PAYMENT,
 	reply_markup=InlineKeyboardMarkup(Data.paymentbuttons)
   )
+
+# Start Message
+@Client.on_message(filters.group & filters.command("payment"))
+@Client.on_message(filters.group & filters.command("payment@PredictorAerobot"))
+async def payment(bot, msg):
+	await msg.reply_photo(
+	random.choice(PAYMENT_IMG),
+	caption=Data.PAYMENT,
+	reply_markup=InlineKeyboardMarkup(Data.paymentbuttons)
+	)
+	
