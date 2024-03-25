2b901ad8298acfecc24228314f427dfcf8dadf84
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-03-25 16:38:59 +0530

    Update fastwin.py

--- PredictorAerobot/fastwin.py
+++ PredictorAerobot/fastwin.py
@@ -1,13 +1,20 @@
 from Data import Data
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message 
-from Data import FASTWIN_IMG
+from Aero import FASTWIN_IMG
 import random 
 
 # Start Message
-@Client.on_message(filters.text & filters.incoming & filters.command("fastwin"))
-@Client.on_message(filters.private & filters.incoming & filters.command("fastwin"))
-@Client.on_message(filters.text & filters.incoming & filters.command("fastwin@PredictorAerobot"))
+@Client.on_message(filters.private & filters.command("fastwin"))
+@Client.on_message(filters.private & filters.command("fastwin@PredictorAerobot"))
+async def fastwin(bot, msg):
+	await msg.reply_photo(
+	random.choice(FASTWIN_IMG),
+	caption=Data.FASTWIN,
+	reply_markup=InlineKeyboardMarkup(Data.fastwinbuttons)
+	)
+@Client.on_message(filters.group & filters.command("fastwin"))
+@Client.on_message(filters.group & filters.command("fastwin@PredictorAerobot"))
 async def fastwin(bot, msg):
 	await msg.reply_photo(
 	random.choice(FASTWIN_IMG),
