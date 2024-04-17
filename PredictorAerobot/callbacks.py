aa92095684e8046a0a021be9f3b00a4776ddf2e1
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-04-17 10:54:08 +0530

    Update callbacks.py

--- PredictorAerobot/callbacks.py
+++ PredictorAerobot/callbacks.py
@@ -204,7 +204,7 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
         await bot.edit_message_text(
             chat_id=chat_id,
             message_id=message_id,
-            text=Data.BET82,
+            text=Data.LOTTERY82,
             disable_web_page_preview=True,                        
             reply_markup=InlineKeyboardMarkup(Data.bet82_buttons),
         )
