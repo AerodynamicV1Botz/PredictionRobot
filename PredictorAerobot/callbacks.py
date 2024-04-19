93b8a52702f3b1d9476fce2340002d8a9eef53cc
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-04-20 00:45:56 +0530

    Update callbacks.py

--- PredictorAerobot/callbacks.py
+++ PredictorAerobot/callbacks.py
@@ -208,13 +208,13 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.goagame_back),
         )
-    elif query == "bgame":
+    elif query == "bg":
         chat_id = callback_query.from_user.id
         message_id = callback_query.message.id
         await bot.edit_message_text(
             chat_id=chat_id,
             message_id=message_id,
-            text=Data.BGAME,
+            text=Data.bg,
             disable_web_page_preview=True,                        
             reply_markup=InlineKeyboardMarkup(Data.bountygame_buttons),
         )
@@ -228,7 +228,7 @@ async def _callbacks(bot: Client, callback_query: CallbackQuery):
             disable_web_page_preview=True,                        
             reply_markup=InlineKeyboardMarkup(Data.bountygame_buttons),
         )
-    elif query == "bet82wingo":
+    elif query == "bountygamewingo":
         chat_id = callback_query.from_user.id
         message_id = callback_query.message.id
         await bot.edit_message_text(
