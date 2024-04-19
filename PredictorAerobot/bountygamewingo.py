15fdf6e615964c7b45ba70427671be27f9bcc320
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-04-20 00:10:49 +0530

    Create bountygamewingo.py

---
+++ PredictorAerobot/bountygamewingo.py
@@ -0,0 +1,16 @@
+from Data import Data
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
+from Data import GOAGAMESIZE
+import random 
+
+# Bounty Game Wingo Prediction 
+async def ggwingogp(bot, msg):
+	user = await bot.get_me()
+	mention = user.mention
+	await msg.reply_photo(
+	random.choice(GOAGAMESIZE),
+	caption=Data.GGRESULT.format(msg.from_user.mention, mention),
+	reply_markup=InlineKeyboardMarkup(Data.goagame_group_buttons)
+	)
+						    
