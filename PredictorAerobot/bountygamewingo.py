37b20f1b66075c934885ab7db4d60b40d7d75dca
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2024-04-20 00:24:17 +0530

    Update bountygamewingo.py

--- PredictorAerobot/bountygamewingo.py
+++ PredictorAerobot/bountygamewingo.py
@@ -1,25 +1,25 @@
 from Data import Data
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
-from Data import GOAGAMESIZE
+from Data import BOUNTYGAMESIZE
 import random 
 
 # Bounty Game Wingo Prediction 
-async def ggwingogp(bot, msg):
+async def bountygamewingogp(bot, msg):
 	user = await bot.get_me()
 	mention = user.mention
 	await msg.reply_photo(
-	random.choice(GOAGAMESIZE),
-	caption=Data.GGRESULT.format(msg.from_user.mention, mention),
-	reply_markup=InlineKeyboardMarkup(Data.goagame_group_buttons)
+	random.choice(BOUNTYGAMESIZE),
+	caption=Data.BOUNTYGAMERESULT.format(msg.from_user.mention, mention),
+	reply_markup=InlineKeyboardMarkup(Data.bountygame_group_buttons)
 	)
 
-async def ggwingo(bot, msg):
+async def bountygamewingo(bot, msg):
 	user = await bot.get_me()
 	mention = user.mention
 	await msg.reply_photo(
-	random.choice(GOAGAMESIZE),
-	caption=Data.GGRESULT.format(msg.from_user.mention, mention),
-	reply_markup=InlineKeyboardMarkup(Data.goagame_result_buttons)
+	random.choice(BOUNTYGAMESIZE),
+	caption=Data.BOUNTYGAMERESULT.format(msg.from_user.mention, mention),
+	reply_markup=InlineKeyboardMarkup(Data.bountygame_result_buttons)
 	)
 						    
